f14790cb3269275a3229b064ad747118cab2eba2
bfsgr <bfsgr.code@protonmail.com>
2021-03-30 18:44:40 -0300

    dados adicionais paciente

--- src/com/uem/clinica/entidades/Paciente.java
+++ src/com/uem/clinica/entidades/Paciente.java
@@ -2,11 +2,19 @@ package com.uem.clinica.entidades;
 
 import com.uem.clinica.util.*;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class Paciente extends Pessoa{
 
     private Convenio convenio;
+
+    private HashMap<String,String> dadosAdicionais;
+
+    private ArrayList<String> cirurgias;
+
+    private ArrayList<String> alergias;
 
     public Paciente(String nome, Calendar dataNascimento, Endereco endereco, String celular, String email, Convenio convenio) {
         super(nome, dataNascimento, endereco, celular, email);
@@ -19,5 +27,29 @@ public class Paciente extends Pessoa{
 
     public void setConvenio(Convenio convenio) {
         this.convenio = convenio;
+    }
+
+    public HashMap<String, String> getDadosAdicionais() {
+        return dadosAdicionais;
+    }
+
+    public void setDadosAdicionais(HashMap<String, String> dadosAdicionais) {
+        this.dadosAdicionais = dadosAdicionais;
+    }
+
+    public ArrayList<String> getCirurgias() {
+        return cirurgias;
+    }
+
+    public void setCirurgias(ArrayList<String> cirurgias) {
+        this.cirurgias = cirurgias;
+    }
+
+    public ArrayList<String> getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(ArrayList<String> alergias) {
+        this.alergias = alergias;
     }
 }

b243f9a6f0010dc40d93230f70a416fa1e49c582
bfsgr <bfsgr.code@protonmail.com>
2021-03-31 13:58:28 -0300

    Remove métodos desnecessários

--- src/com/uem/clinica/entidades/Secretaria.java
+++ src/com/uem/clinica/entidades/Secretaria.java
@@ -19,34 +19,6 @@ public class Secretaria extends Pessoa {
         super(nome, dataNascimento, endereco, celular, email);
     }
 
-
-    public Secretaria criarSecretaria(String nome, LocalDate dataNascimento, Endereco endereco, String celular) {
-        return new Secretaria(nome, dataNascimento, endereco, celular);
-    }
-
-    public Secretaria criarSecretaria(String nome, LocalDate dataNascimento, Endereco endereco, String celular, String email) {
-        return new Secretaria(nome, dataNascimento, endereco, celular, email);
-    }
-
-    public void atualizarSecretaria(Secretaria secretaria, String nome, LocalDate dataNascimento, Endereco endereco, String celular) {
-        secretaria.setNome(nome);
-        secretaria.setDataNascimento(dataNascimento);
-        secretaria.setEndereco(endereco);
-        secretaria.setCelular(celular);
-    }
-
-    public void atualizarPaciente(Secretaria secretaria, String nome, LocalDate dataNascimento, Endereco endereco, String celular, String email) {
-        secretaria.setNome(nome);
-        secretaria.setDataNascimento(dataNascimento);
-        secretaria.setEndereco(endereco);
-        secretaria.setCelular(celular);
-        secretaria.setEmail(email);
-    }
-
-    public void removerSecretaria(Secretaria secretaria) {
-        secretaria = null;
-    }
-
     public Paciente criarPaciente(String nome, LocalDate dataNascimento, Endereco endereco, String celular, Convenio convenio) {
         return new Paciente(nome, dataNascimento, endereco, celular, convenio);
     }

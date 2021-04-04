d3d17737489f9ed95865a565dbf732ba32dfe157
bfsgr <bfsgr.code@protonmail.com>
2021-04-04 12:51:36 -0300

    instancia arranjos nos construtores

--- src/com/uem/clinica/entidades/Consulta.java
+++ src/com/uem/clinica/entidades/Consulta.java
@@ -13,13 +13,14 @@ public class Consulta {
     private TipoConsulta tipo;
 
     public Consulta(){
-
+        prontuario = new Prontuario();
     }
 
     public Consulta(LocalDateTime data, Paciente paciente, TipoConsulta tipo) {
         this.data = data;
         this.paciente = paciente;
         this.tipo = tipo;
+        prontuario = new Prontuario();
     }
 
     public int getId() {
--- src/com/uem/clinica/entidades/Prontuario.java
+++ src/com/uem/clinica/entidades/Prontuario.java
@@ -9,13 +9,16 @@ public class Prontuario {
     private HashMap<String, String> prescricao;
 
     public Prontuario(){
-
+        sintomas = new ArrayList<>();
+        prescricao = new HashMap<>();
     }
 
     public Prontuario(ArrayList<String> sintomas, String diagnostico, HashMap<String, String> prescricao) {
         this.sintomas = sintomas;
         this.diagnostico = diagnostico;
         this.prescricao = prescricao;
+        sintomas = new ArrayList<>();
+        prescricao = new HashMap<>();
     }
 
     public ArrayList<String> getSintomas() {
